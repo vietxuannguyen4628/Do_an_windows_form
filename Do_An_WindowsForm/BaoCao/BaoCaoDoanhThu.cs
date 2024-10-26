a3700b7aef6958dfde36972fc421a3e8f8f1b98f
hikaru2561 <nguyenkimquang2561@gmail.com>
2024-10-26 09:49:19 +0700

    Update BaoCao

---
+++ Do_An_WindowsForm/BaoCao/BaoCaoDoanhThu.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Do_An_WindowsForm.BaoCao
+{
+    public partial class BaoCaoDoanhThu : DevExpress.XtraEditors.XtraUserControl
+    {
+        public BaoCaoDoanhThu()
+        {
+            InitializeComponent();
+        }
+
+        private void BaoCaoDoanhThu_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
